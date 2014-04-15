40933b8f7fbdf8de0f199b9a6ed42defddda0716
mbark <barksten@kth.se>
2014-04-15 16:30:42 +0200

    Add a new leiningen project for clojure

---
+++ src/main/clojure/poker-bot/src/poker_bot/core.clj
@@ -0,0 +1,84 @@
+(ns poker-bot.core
+  (:gen-class))
+(import 'se.cygni.texasholdem.player.Player)
+
+(declare poker-bot)
+(declare get-best-action)
+
+(defn -main
+  []
+  (.playATrainingGame
+   (poker-bot
+    "poker.cygni.se"
+    4711)))
+
+(defn poker-bot
+  [host port]
+  (proxy [Player] []
+    (getName
+     []
+     "ClojureBot")
+    (actionRequired
+     [request]
+     (get-best-action request))
+    (onPlayIsStarted
+     [event]
+     )
+    (onTableChangedStateEvent
+     [event]
+     )
+    (onYouHaveBeenDealtACard
+     [event]
+     )
+    (onCommunityHasBeenDealtACard
+     [event]
+     )
+    (onPlayerBetBigBlind
+     [event]
+     )
+    (onPlayerBetSmallBlind
+     [event]
+     )
+    (onPlayerFolded
+     [event]
+     )
+    (onPlayerForcedFolded
+     [event]
+     )
+    (onPlayerCalled
+     [event]
+     )
+    (onPlayerRaised
+     [event]
+     )
+    (onTableIsDone
+     [event]
+     )
+    (onPlayerChecked
+     [event]
+     )
+    (onYouWonAmount
+     [event]
+     )
+    (onShowDown
+     [event]
+     )
+    (onPlayerQuit
+     [event]
+     )
+    (connectionToGameServerLost
+     [event]
+     )
+    (connectionToGameServerEstablished
+     [event]
+     )
+    (serverIsShuttingDown
+     [event]
+     )
+    ))
+
+(defn get-best-action
+  [request]
+  (first
+   (.getPossibleActions
+    request)))
