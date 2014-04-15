0438cdebd27223ca5e20cb4a0716ff8162221162
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2014-04-15 18:29:37 +0200

    Began work on poker logic.

---
+++ pseudo-lisp.clj
@@ -0,0 +1,60 @@
+(defn todo []
+  (throw (Exception. "TODO")))
+
+(def suits #{:heart :spade :diamond :clubs})
+
+(def ranks #{:ace :two :three :four :five :six :seven
+             :eight :nine :ten :jack :queen :king})
+
+(def full-deck
+  (set (for [suit suits
+        rank ranks]
+    {:suit suit :rank rank})))
+
+(defn action [game-state]
+  (if (can-check? game-state)
+    :check
+    (if (<= (pot-odds game-state) (hand-odds game-state))
+      :fold
+      :call)))
+
+(defn pot-odds [game-state]
+  (/ 1 (+ (pot-size game-state)
+          (amount-needed-to-call game-state))))
+
+(defn hand-odds [game-state]
+  (let [odds (dec (/ (number-of-unseen-cards game-state)
+                  (number-of-outs game-state)))]
+    (if (flop? game-state)
+      (* odds 4)
+      (if (turn? game-state)
+        (* odds 2)
+        odds))))
+
+(defn can-check? [game-state]
+  (todo))
+
+(defn flop? [game-state]
+  (todo))
+
+(defn turn? [game-state]
+  (todo))
+
+(defn cards-on-hand [game-state]
+  (todo))
+
+(defn cards-on-table [game-state]
+  (todo))
+
+(defn number-of-unseen-cards [game-state]
+  (todo)
+
+(defn number-of-outs [game-state]
+  (todo))
+
+(defn pot-size [game-state]
+  (todo))
+
+(defn amount-needed-to-call [game-state]
+  (todo))
+
